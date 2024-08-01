4b5192e96cba8246bcd7e755ce6e2341c31f7d24
Jose Luis Escolá García <jletrabajoapple@gmail.com>
2024-08-01 17:29:18 +0200

    Initial Commit

---
+++ DoomKanban/DoomKanbanApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoomKanbanApp.swift
+//  DoomKanban
+//
+//  Created by Jose Luis Escolá García on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct DoomKanbanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
